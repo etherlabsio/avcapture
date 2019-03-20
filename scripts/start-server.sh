02845189f8249b1bbe7f7d720232fbbdedeb4762
Lijo Jose <lijo@etherlabs.io>
2019-03-20 14:17:06 +0530

    add check for ffmpeg tgz file

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-cd /tmp
-wget $FFMPEG_TGZ_URI &&  tar -xzf ubuntu16.04.tgz && mv bin/ffmpeg /usr/local/bin && mv lib/* /usr/local/lib/ && rm ubuntu16.04.tgz && /bin/server
+WORK_DIR=/data
+cd $WORK_DIR
+
+if [ -z $UPDATE_FFMPEG_CACHE ]
+then
+    UPDATE_FFMPEG_CACHE="1"
+fi 
+
+if [ ! -f $WORK_DIR/ffmpeg.tgz ] || [ $UPDATE_FFMPEG_CACHE -eq "1" ]
+then 
+    wget $FFMPEG_TGZ_URI -O ffmpeg.tgz
+    wget $FFMPEG_DEPS_URI -O ffmpeg-deps.tgz
+fi
+
+if [ ! -f $WORK_DIR/ffmpeg.tgz ]
+then 
+    echo "failed to fetch ffmpeg binary"
+    exit 
+fi
+if [ ! -f $WORK_DIR/ffmpeg-deps.tgz ]
+then 
+    echo "failed to fetch ffmpeg dependencies"
+    exit 
+fi
+
+tar -xzf ffmpeg.tgz && mv ffmpeg /usr/local/bin
+tar -xzf ffmpeg-deps.tgz &&  mv lib/* /usr/local/lib/ 
+/bin/server
